b8f3ec8fcc3315084e9b4d1ce75a1a30362250f0
Shahriyar Rzayev <rzayev.sehriyar@gmail.com>
2018-03-27 17:58:03 +0400

    again increased again and again :)

--- prepare_env_test_mode/take_backup.py
+++ prepare_env_test_mode/take_backup.py
@@ -329,7 +329,7 @@ class WrapperForBackupTest(Backup):
                 #                                             sql="select benchmark(9999999, md5(c)) from sysbench_test_db.sbtest{}".format(
                 #                                                 i)))
 
-                sleep(120)
+                sleep(150)
                 self.all_backup()
                     # self.check_kill_process('call_ddl_test')
         except Exception as err:
